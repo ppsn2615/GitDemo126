82d7e62a27774edab0474b7b2120ced1fb3f5205
ppsn2615 <psn2615@gmail.com>
2023-04-06 16:17:24 +0900

    If문으로 성적 구하기

---
+++ src/test/Test1.java
@@ -0,0 +1,36 @@
+package test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+	
+		String name = "김태희";
+		int score = 75;
+		char grade = ' ';
+		
+		if ((score>=90)&&(score<=100)) {
+			grade = 'A';
+			System.out.println("점수 : "+score+"\t학점 : "+grade );
+		}
+		else if((score>=80)&&(score<90)) {
+			grade = 'B';
+			System.out.println("점수 : "+score+"\t학점 : "+grade );
+		}
+		else if((score>=70)&&(score<80)) {
+			grade = 'C';
+			System.out.println("점수 : "+score+"\t학점 : "+grade );
+		}
+		else if((score>=60)&&(score<70)) {
+			grade = 'D';
+			System.out.println("점수 : "+score+"\t학점 : "+grade );
+		}
+		else if((score>0)&&(score<60)) {
+			grade = 'F';
+			System.out.println("점수 : "+score+"\t학점 : "+grade );
+		}
+		else {
+			System.out.println("잘못된 점수입니다.");
+		}
+		
+	}
+}
