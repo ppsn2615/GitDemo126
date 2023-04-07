3b60e2acd6e889c551b3eb7b2a5551affb01a1cb
ppsn2615 <psn2615@gmail.com>
2023-04-07 10:56:01 +0900

    [BranchX] Test1.java에 branch_name 추가

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -7,30 +7,28 @@ public class Test1 {
 		String name = "김태희";
 		int score = 75; 
 		char grade = ' ';
+		String branch_name = "BrahcnX";
 		
 		if ((score>=90)&&(score<=100)) {
 			grade = 'A';
-			System.out.println("점수 : "+score+"\t학점 : "+grade );
 		}
 		else if((score>=80)&&(score<90)) {
 			grade = 'B';
-			System.out.println("점수 : "+score+"\t학점 : "+grade );
 		}
 		else if((score>=70)&&(score<80)) {
 			grade = 'C';
-			System.out.println("점수 : "+score+"\t학점 : "+grade );
 		}
 		else if((score>=60)&&(score<70)) {
 			grade = 'D';
-			System.out.println("점수 : "+score+"\t학점 : "+grade );
 		}
 		else if((score>0)&&(score<60)) {
 			grade = 'F';
-			System.out.println("점수 : "+score+"\t학점 : "+grade );
 		}
 		else {
 			System.out.println("잘못된 점수입니다.");
 		}
 		
+		System.out.println("점수 : "+score+"\t학점 : "+grade );
+		System.out.println("branch명 : " + branch_name);
 	}
 }
